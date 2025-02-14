a7c491eae084bf582424df3cf909b761f8a3ba40
Milan <milan.berta@e.email>
2025-02-14 22:47:47 +0100

    chore: renamed the symbols to match the https://happyglimpse.itch.io/endless-puzzle project

--- def_tiles.py
+++ def_tiles.py
@@ -1,14 +1,29 @@
 # Tile definitions
 # First letter: Tube, Ball, Crystal, Layer
 # Second letter: Small vs. Large, Dark vs. Light
+#tiles = [
+#    ("TS", "BL", "CS", "LS"),
+#    ("LL", "TS", "CL", "BD"),
+#    ("CL", "BL", "LL", "TL"), #("CL", "BD", "LL", "TL"),
+#    ("CL", "LS", "BD", "TS"),
+#    ("CS", "TS", "CS", "LL"),
+#    ("LS", "CS", "BL", "TL"), #("LS", "CS", "BD", "TL"),
+#    ("LS", "BD", "TL", "CL"),
+#    ("TL", "BL", "LS", "BL"),
+#    ("TS", "LS", "CS", "BD"),
+#]
+
+
+# First letter: Triangle, Circle, Ellipsis, Line
+# Second letter: Small vs. Large
 tiles = [
-    ("TS", "BL", "CS", "LS"),
-    ("LL", "TS", "CL", "BD"),
-    ("CL", "BL", "LL", "TL"), #("CL", "BD", "LL", "TL"),
-    ("CL", "LS", "BD", "TS"),
-    ("CS", "TS", "CS", "LL"),
-    ("LS", "CS", "BL", "TL"), #("LS", "CS", "BD", "TL"),
-    ("LS", "BD", "TL", "CL"),
-    ("TL", "BL", "LS", "BL"),
-    ("TS", "LS", "CS", "BD"),
+    ("CS", "LS", "TS", "ES"),
+    ("EL", "CS", "TL", "LL"),
+    ("TL", "LS", "EL", "CL"),
+    ("TL", "ES", "LL", "CS"),
+    ("TS", "CS", "TS", "EL"),
+    ("ES", "TS", "LS", "CL"),
+    ("ES", "LL", "CL", "TL"),
+    ("CL", "LS", "ES", "LS"),
+    ("CS", "ES", "TS", "LL"),
 ]
